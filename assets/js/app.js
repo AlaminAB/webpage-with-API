4f34c6ebfd3f50fdf645a097201a51e32148e73a
AlaminAB <alaminislam907@gmail.com>
2020-02-09 15:29:23 +0600

    diff box

--- assets/js/app.js
+++ assets/js/app.js
@@ -23,6 +23,15 @@ $(document).ready(function(){
 
 
 // button 2
+ $.ajax({url: "https://randomuser.me/api/", success: function(result){
+      // insert API value in html tag 
+        $('.img-src1').attr('src',result.results[0].picture.large);
+      $(".name1").html(result.results[0].name.first)
+      $(".gender1").html(result.results[0].gender)
+      $(".city1").html(result.results[0].location.city)
+      $(".email1").html(result.results[0].email);
+    }});
+
    $(".button1 > button").click(function(){
      $.ajax({url: "https://randomuser.me/api/", success: function(result){
       // insert API value in html tag 
@@ -37,6 +46,15 @@ $(document).ready(function(){
 
 
 // button 2
+ $.ajax({url: "https://randomuser.me/api/", success: function(result){
+      // insert API value in html tag 
+        $('.img-src2').attr('src',result.results[0].picture.large);
+      $(".name2").html(result.results[0].name.first)
+      $(".gender2").html(result.results[0].gender)
+      $(".city2").html(result.results[0].location.city)
+      $(".email2").html(result.results[0].email);
+    }});
+
     $(".button2 > button").click(function(){
      $.ajax({url: "https://randomuser.me/api/", success: function(result){
       // insert API value in html tag 
@@ -52,6 +70,15 @@ $(document).ready(function(){
 
 
 // button 3
+ $.ajax({url: "https://randomuser.me/api/", success: function(result){
+      // insert API value in html tag 
+        $('.img-src3').attr('src',result.results[0].picture.large);
+      $(".name3").html(result.results[0].name.first)
+      $(".gender3").html(result.results[0].gender)
+      $(".city3").html(result.results[0].location.city)
+      $(".email3").html(result.results[0].email);
+    }});
+    
    $(".button3 > button").click(function(){
      $.ajax({url: "https://randomuser.me/api/", success: function(result){
       // insert API value in html tag 
